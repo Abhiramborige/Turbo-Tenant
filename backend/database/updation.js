cc6ff761b8e6b5d851cbe0689a8623adff62ca3a
mohan0917 <71814973+mohan0917@users.noreply.github.com>
2021-11-23 22:08:29 +0530

    Update updation.js

--- backend/database/updation.js
+++ backend/database/updation.js
@@ -12,6 +12,61 @@ export function update_ownertable(object, query){
     if (err) throw err;
     console.log(result.affectedRows + " record(s) updated");
   });
+
+  export function update_TenantTable(object, query){
+  var sql = "UPDATE TenantTable SET"
+  for (const [key, value] of Object.entries(object)) {
+    sql+=`${value} WHERE ${key} =`
+  }
+  for (const [key, value] of Object.entries(query)) {
+    sql+=`${value}`
+  }
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result.affectedRows + " record(s) updated");
+  });
+
+
+  export function update_BookingTable(object, query){
+  var sql = "UPDATE BookingTable SET"
+  for (const [key, value] of Object.entries(object)) {
+    sql+=`${value} WHERE ${key} =`
+  }
+  for (const [key, value] of Object.entries(query)) {
+    sql+=`${value}`
+  }
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result.affectedRows + " record(s) updated");
+  });
+
+
+  export function update_HouseTable(object, query){
+  var sql = "UPDATE HouseTable SET"
+  for (const [key, value] of Object.entries(object)) {
+    sql+=`${value} WHERE ${key} =`
+  }
+  for (const [key, value] of Object.entries(query)) {
+    sql+=`${value}`
+  }
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result.affectedRows + " record(s) updated");
+  });
+
+
+  export function update_MembersTable(object, query){
+  var sql = "UPDATE MembersTable SET"
+  for (const [key, value] of Object.entries(object)) {
+    sql+=`${value} WHERE ${key} =`
+  }
+  for (const [key, value] of Object.entries(query)) {
+    sql+=`${value}`
+  }
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result.affectedRows + " record(s) updated");
+  });
 }
 
 
