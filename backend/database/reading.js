ac4ffb71b0985f18caf1ee6c32cadfb6818c75b7
mohan0917 <71814973+mohan0917@users.noreply.github.com>
2021-11-23 22:14:27 +0530

    Update reading.js

--- backend/database/reading.js
+++ backend/database/reading.js
@@ -7,6 +7,41 @@ export function read_ownertable(){
     if (err) throw err;
     return result;
   });
+
+export function read_TenantTable(){
+  var sql="SELECT * FROM TenantTable";
+  con.query(sql, function (err, result, fields) {
+    if (err) throw err;
+    return result;
+  });
+
+export function read_BookingTable(){
+  var sql="SELECT * FROM BookingTable";
+  con.query(sql, function (err, result, fields) {
+    if (err) throw err;
+    return result;
+  });
+
+
+export function read_HouseTable(){
+  var sql="SELECT * FROM HouseTable";
+  con.query(sql, function (err, result, fields) {
+    if (err) throw err;
+    return result;
+  });
+
+export function read_MembersTable(){
+  var sql="SELECT * FROM MembersTable";
+  con.query(sql, function (err, result, fields) {
+    if (err) throw err;
+    return result;
+  });
+
+
+  
+  
+
+
 }
 
 
